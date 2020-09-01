426454523c4b72fc4a9d3c68e745e0f3b7013c40
john-waczak <john.louis.waczak@gmail.com>
2020-08-31 22:07:26 -0500

    added new facial landmark visualization

--- visualization/landmarks-side_by_side.py
+++ visualization/landmarks-side_by_side.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import json
 from tqdm import tqdm
 
@@ -15,12 +16,21 @@ fps = 15  # determined by ffmpeg
 
 with open('../landmarks.json') as json_file:
     landmarks = json.load(json_file)
-    plt.figure()
-    for frame, faces in landmarks.items():
-        plt.cla()
-        plt.title("Frame number: {}".format(frame))
+
+    for frame, faces in tqdm(landmarks.items()):
+        file_name = "../frames/out{:05d}.png".format(int(frame)+1)
         for face, points in faces.items():
             if face == "0":
+
+                fig, ax = plt.subplots(1, 2)
+                fig.suptitle("Frame number: {}".format(frame))
+                # first plot the regular image
+                img = mpimg.imread(file_name)
+                ax[0].imshow(img)
+                ax[0].set_title("Original")
+                ax[0].axis('off')
+                ax[0].grid('off')
+
                 points = np.array(points)[0]
                 points = points
                 x = points[:, 0]
@@ -47,32 +57,35 @@ with open('../landmarks.json') as json_file:
 
                 # NOTE array slices do NOT include final index
                 # jaw
-                plt.plot(x[0:16+1], y[0:16+1], color='b')
+                ax[1].plot(x[0:16+1], y[0:16+1], color='b')
                 # right eyebrow
-                plt.plot(x[17:21+1], y[17:21+1], color='b')
+                ax[1].plot(x[17:21+1], y[17:21+1], color='b')
                 # left eyebrow
-                plt.plot(x[22:26+1], y[22:26+1], color='b')
+                ax[1].plot(x[22:26+1], y[22:26+1], color='b')
                 # nose
-                plt.plot(np.concatenate((x[27:30+1], [x[33]])), np.concatenate((y[27:30+1], [y[33]])), color='b')
+                ax[1].plot(np.concatenate((x[27:30+1], [x[33]])), np.concatenate((y[27:30+1], [y[33]])), color='b')
                 # nostrils
-                plt.plot(x[31:35+1], y[31:35+1], color='b')
+                ax[1].plot(x[31:35+1], y[31:35+1], color='b')
                 # right eye
-                plt.plot(np.concatenate((x[36:41+1], [x[36]])), np.concatenate((y[36:41+1], [y[36]])), color='b')
+                ax[1].plot(np.concatenate((x[36:41+1], [x[36]])), np.concatenate((y[36:41+1], [y[36]])), color='b')
                 # left eye
-                plt.plot(np.concatenate((x[42:47+1], [x[42]])), np.concatenate((y[42:47+1], [y[42]])), color='b')
+                ax[1].plot(np.concatenate((x[42:47+1], [x[42]])), np.concatenate((y[42:47+1], [y[42]])), color='b')
                 # upper lip, upper line
-                plt.plot(x[48:54+1], y[48:54+1], color='b')
+                ax[1].plot(x[48:54+1], y[48:54+1], color='b')
                 # lower lip, lower line
-                plt.plot(x[55:59+1], y[55:59+1], color='b')
+                ax[1].plot(x[55:59+1], y[55:59+1], color='b')
                 # upper lip, lower line
-                plt.plot(x[60:64+1], y[60:64+1], color='b')
+                ax[1].plot(x[60:64+1], y[60:64+1], color='b')
                 # lower lip, upper line
-                plt.plot(x[65:], y[65:], color='b')
+                ax[1].plot(x[65:], y[65:], color='b')
 
 
 
+                ax[1].set_title('Landmarks')
+                ax[1].set_xlim(-1, 1)
+                ax[1].set_ylim(-1, 1)
+                ax[1].set_aspect('equal')
+                ax[1].grid('off')
+                ax[1].axis('off')
 
-                plt.xlim(-1, 1)
-                plt.ylim(-1, 1)
-                plt.gca().set_aspect('equal')
-                plt.pause(1e-9)
+                plt.savefig("./frames/out-{:04d}.png".format(int(frame)+1))
---
+++ visualization/stills-to-movie.sh
@@ -0,0 +1,1 @@
+ffmpeg -i ./frames/out-%04d.png -r 10 output.mp4
